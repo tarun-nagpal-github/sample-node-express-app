7c083786f8e927b9a952045a4b6375f757cd836d
tarun_nagpal@mckinsey.com <tarun_nagpal@mckinsey.com>
2021-01-14 15:23:04 +0530

    Added IP Service

--- app.js
+++ app.js
@@ -9,15 +9,40 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+/*
+   Get the data using Fetch. 
+   It will fetch the random activity for tasks suggestion
+*/
+  
 app.get('/show-me-random-activity', function(req, res){
-
-  // Get the data using Fetch. It will fetch the random activity for tasks suggestion
   fetch('https://www.boredapi.com/api/activity')
   .then(response => response.json())
   .then(data => {
     res.send(data);
   })
-})
+
+});
+
+//  It will fetch the random Image
+app.get('/show-me-random-image', function(req, res){
+  fetch('https://dog.ceo/api/breeds/image/random')
+  .then(response => response.json())
+  .then(data => {
+    res.send(data);
+  })
+});
+
+
+// Get your IP Address
+app.get('/what-is-my-ip', function(req, res){
+  fetch('https://api.ipify.org/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    res.send(data);
+  })
+});
+
+
 
 // Listen
 var port = process.env.PORT || 3000;
